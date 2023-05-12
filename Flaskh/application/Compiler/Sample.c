92b3f293ee85f3b37cd1241dd8dc32f7051ae10a
Sarvajnya <pujarsarvajnya5@gmail.com>
2023-05-12 19:57:59 +0530

    recent

--- Flaskh/application/Compiler/Sample.c
+++ Flaskh/application/Compiler/Sample.c
@@ -1,49 +1,31 @@
-//Multiplication
-#include<stdio.h>    
-#include<stdlib.h>  
-int main(){  
-int a[10][10],b[10][10],mul[10][10],r,c,i,j,k;    
-printf("enter the number of row");    
-scanf("%d",&r);    
-printf("enter the number of column");    
-scanf("%d",&c);    
-printf("enter the first matrix element\n");    
-for(i=0;i<r;i++)    
-{    
-for(j=0;j<c;j++)    
-{    
-scanf("%d",&a[i][j]);    
-}    
-}    
-printf("enter the second matrix element\n");    
-for(i=0;i<r;i++)    
-{    
-for(j=0;j<c;j++)    
-{    
-scanf("%d",&b[i][j]);    
-}    
-}    
-    
-printf("multiply of the matrix\n");    
-for(i=0;i<r;i++)    
-{    
-for(j=0;j<c;j++)    
-{    
-mul[i][j]=0;    
-for(k=0;k<c;k++)    
-{    
-mul[i][j]+=a[i][k]*b[k][j];    
-}    
-}    
-}    
-//for printing result    
-for(i=0;i<r;i++)    
-{    
-for(j=0;j<c;j++)    
-{    
-printf("%d\t",mul[i][j]);    
-}    
-printf("\n");    
-}    
-return 0;  
-} 
+//Prime
+#include <stdio.h>
+
+int main() {
+
+  int n, i, flag = 0;
+  scanf("%d", &n);
+
+  // 0 and 1 are not prime numbers
+  // change flag to 1 for non-prime number
+  if (n == 0 || n == 1)
+    flag = 1;
+
+  for (i = 2; i <= n / 2; ++i) {
+
+    // if n is divisible by i, then n is not prime
+    // change flag to 1 for non-prime number
+    if (n % i == 0) {
+      flag = 1;
+      break;
+    }
+  }
+
+  // flag is 0 for prime numbers
+  if (flag == 0)
+    printf("%d is a prime number.", n);
+  else
+    printf("%d is not a prime number.", n);
+
+  return 0;
+}
